3c9eb347de257a8ec2cb988b0c6f44adf8b81943
Damon Burda <damonburda96@gmail.com>
2021-08-20 17:42:42 -0700

    footer component

---
+++ src/components/footer.js
@@ -0,0 +1,11 @@
+function footer() {
+    return (
+        <footer class="navbar fixed-bottom navbar-light bg-light">
+          <h2 class="navbar-text">
+            Damon1v1 on Github
+          </h2>
+      </footer>
+    );
+};
+
+export default footer;
