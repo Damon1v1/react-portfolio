07f6369283fc4b0a506db6390cc39da206718fcc
Damon Burda <damonburda96@gmail.com>
2021-08-23 14:16:33 -0700

    about me page skeleton

--- src/pages/aboutme/aboutme.js
+++ src/pages/aboutme/aboutme.js
@@ -1,13 +1,10 @@
 import  React from "react";
 import { Row, Col } from "react-bootstrap";
 import "./aboutme.css";
-import "../../components/header/Header";
 import "../../components/about/About";
-import "../../components/footer/Footer";
-import about from "../../components/about/About";
 
 function Aboutme() {
     <Row className="justify-content-center">
-        
+        <Aboutme></Aboutme>
     </Row>
 }
