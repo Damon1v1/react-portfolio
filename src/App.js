cafd3048bf70d49bc46ac4d99de8c03dbd0fe985
Damon Burda <damonburda96@gmail.com>
2021-08-23 15:16:41 -0700

    Basic app structure and beginning routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
+import Aboutme from "./pages/aboutme/aboutme";
+import Header from "./components/header/header";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header/>
+      <div className="container">
+        <Switch>
+          <Route exact path="aboutme" component={Aboutme} />
+        </Switch>
+      </div>
+      
+    </Router>
   );
 }
 
