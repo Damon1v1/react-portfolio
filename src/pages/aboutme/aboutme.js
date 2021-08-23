8305d3bf8555dbfea6a49f7f4a6c6d6f2cce52f9
Damon Burda <damonburda96@gmail.com>
2021-08-23 14:06:39 -0700

    importing components into home page

--- src/pages/aboutme/aboutme.js
+++ src/pages/aboutme/aboutme.js
@@ -1,3 +1,12 @@
 import  React from "react";
 import { Row, Col } from "react-bootstrap";
 import "./aboutme.css";
+import "../../components/header/header";
+import "../../components/about/about";
+import "../../components/footer/footer";
+
+function Aboutme() {
+    <Row className="justify-content-center">
+
+    </Row>
+}
