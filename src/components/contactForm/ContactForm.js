821e173a207db498fb2baf087062af62b0b89855
Damon Burda <damonburda96@gmail.com>
2021-08-26 15:44:25 -0700

    contact field names match email template

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -22,19 +22,19 @@ function Contact() {
         <div class="mb-3">
           <label for="exampleInputName1" class="col-sm-2 form-label">Name</label>
           <div class="col-sm-5">
-            <input class="form-control" id="nameInput" placeholder="Name"/>
+            <input class="form-control" id="nameInput" placeholder="Name" name="name"/>
           </div>
         </div>
         <div class="mb-3">
           <label id="exampleInputEmail" class="col-sm-2 form-label">E-mail</label>
           <div class="col-sm-5">
-            <input type="email" class="form-control" id="emailInput"  placeholder="E-mail"/>
+            <input type="email" class="form-control" id="emailInput"  placeholder="E-mail" name="email"/>
           </div>
         </div>
         <div class="mb-3">
           <label id="exampleInputMessage1" class="col-sm-2 form-label">Message</label>
           <div class="col-sm-5">
-            <textarea class="form-control" rows="3" id="messageInput" placeholder="Message"></textarea>
+            <input class="form-control" rows="3" id="messageInput" placeholder="Message" name="message" />
           </div>
         </div>
         <button type="submit" class="btn btn-dark">Submit</button>
