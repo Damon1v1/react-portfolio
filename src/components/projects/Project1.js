140f478cea8fec1190d2c1ae17619e31ee60adf9
Damon Burda <damonburda96@gmail.com>
2021-08-27 15:30:00 -0700

    Project1 pane state established

---
+++ src/components/projects/Project1.js
@@ -0,0 +1,11 @@
+import React, {useState} from "react";
+import SlidingPane from "react-sliding-pane";
+import "react-sliding-pane/dist/react-sliding-pane.css";
+import "./Pane.css";
+
+const Project1 = () => {
+    const [state, setState] = useState({
+        isPaneOpen: false,
+    });
+    
+};
