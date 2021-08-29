910c70d89d285954e3b39ecece209395e047401e
Damon Burda <damonburda96@gmail.com>
2021-08-28 21:04:48 -0700

    sliding pane component

--- src/components/projects/Project1.js
+++ src/components/projects/Project1.js
@@ -4,8 +4,28 @@ import "react-sliding-pane/dist/react-sliding-pane.css";
 import "./Pane.css";
 
 const Project1 = () => {
+    //this state might be for if another pane component is also in use
+    //might be a source for error in the future
     const [state, setState] = useState({
         isPaneOpen: false,
     });
-    
+    return (
+        <div>
+            <div>
+                <button onClick={() => setState({ isPaneOpenBottom: true})}>
+                    Click here for details
+                </button>
+            </div>
+            <SlidingPane 
+            closeIcon={<div>X</div>}
+            className="close"
+            isOpen={state.isPaneOpenBottom}
+            title="Movieferee"
+            from="bottom"
+            width="100%"
+            onRequestClose={() => setState({ isPaneOpenBottom: false })}>
+                
+            </SlidingPane>
+        </div>
+    )
 };
