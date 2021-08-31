1303e23791a40759df157a3f76e2abc921e5ea5b
Damon Burda <damonburda96@gmail.com>
2021-08-31 02:44:24 -0700

    fixed html

--- src/pages/portfolio/portfolio.js
+++ src/pages/portfolio/portfolio.js
@@ -5,7 +5,32 @@ import Project2 from "../../components/projects/Project2"
 import Project3 from "../../components/projects/project3"
 
 function Portfolio() {
+    return(
+            <div class="row col-md-8">
+                <article class="block">
+                    <h1 class="block-header">Portfolio</h1>
+                    <hr />
+                    
+                    <div class="row">
+                      <h2 class="col-sm-12" id="project-title">Movie Recommender</h2>
+                      <div class="col-lg-12"></div>
+                      <img src="images/project1.png" alt="Movie Recommender" class="img-fluid port-image" id="project-img" />
+                      <p id="project-description">Application that reccomends movies to the user to watch based on questionnaire responses;
+                        you can view the respository and deployment of the live application 
+                       
+                      </p>
+                    </div>
 
+                    <div class="row">
+                      <h2 class="col-sm-12" id="project-title">Release The Kringle!</h2>
+                      <div class="col-lg-12"></div>
+                      <img src="images/project2.png" alt="Release The Kringle home page" class="img-fluid port-image" id="project2-img" />
+                      <p id="project-description"> Mock-up application for a bakery. Allows users to sign in for rewards to be used on
+                        various kringle products. Check out our repo and deployment
+                    </p>
+                    </div>
+                </article>
+    )
 };
 
 export default Portfolio;
