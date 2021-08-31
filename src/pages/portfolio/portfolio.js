cb8bf64fcd2d000107009b4b3a4bbcd0805da420
Damon Burda <damonburda96@gmail.com>
2021-08-30 17:10:58 -0700

    portfolio page file importing panes

---
+++ src/pages/portfolio/portfolio.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./oortfolio.css"
+import Project1 from "../../components/projects/Project1"
+import Project2 from "../../components/projects/Project2"
+import Project3 from "../../components/projects/project3"
+
+function Portfolio() {
+
+};
+
+export default Portfolio;
