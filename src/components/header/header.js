0d6cec5077936d19d192260be748b7101a0a4351
Damon Burda <damonburda96@gmail.com>
2021-09-06 14:07:17 -0700

    Converted navbar from the cdn version to bootstrap

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,32 +1,22 @@
 import React from "react";
-//import 'bootstrap/dist/css/bootstrap.min.css';
+import { Nav, Navbar, Container } from "react-bootstrap";
 import "./header.css";
 
 function Header() {
     return (
-        <nav className="navbar navbar-expand-xl navbar-dark">
-        <div className="container">
-          <a className="navbar-brand" href="index.html">Damon Burda</a>
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav ml-auto mt-0 mt-lg-1">
-              <li className="nav-item active">
-                <a className="nav-link" href="/">About</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/portfolio">Portfolio</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/contact">Contact</a>
-              </li>
-            </ul>
-          </div>
-          {/* <!--End list--> */}
-        </div>
-        {/* <!--End container--> */}
-      </nav>
+        <Navbar bg="light" expand="lg">
+        <Container>
+        <Navbar.Brand href="#home">Damon Burda</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+            <Nav.Link href="/">About Me</Nav.Link>
+            <Nav.Link href="/portfolio">Portfolio</Nav.Link>
+            <Nav.Link href="/contact">Contact</Nav.Link>
+      </Nav>
+    </Navbar.Collapse>
+  </Container>
+</Navbar>
     )
 }
 
