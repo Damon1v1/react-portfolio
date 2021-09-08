91767bb416667695822a12b4e7caca93c7c53938
Damon Burda <damonburda96@gmail.com>
2021-09-07 20:15:30 -0700

    using react bootstrap version of the card component

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -1,32 +1,25 @@
 import React from "react";
+import {Card, Button} from "react-bootstrap";
 import "./about.css";
 
 function About() {
     return (
-        <main className="container">
-        <div className="card">
-            <div className="row g-0">
-              <div className="">
-                <img className="portrait" src="images/artwork6.png" alt="Self-portrait" />
-              </div>
-              <div className="">
-                <div className="card-body">
-                  <h5 className="card-title">About Me</h5>
-                  <p className="card-text">Web developer from a food service background consolidating leadership, teamwork skills, 
+      <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+                    Web developer from a food service background consolidating leadership, teamwork skills, 
                     and quick problem solving ability from a fast-paced work environment to build a sleek and 
                     intuitive user experience on the web. Helped my restaurant achieve the fastest service times company wide 
                     in the third fiscal quarter of 2020 and number one overall restaurant in quarter four. I enjoy working in an 
                     environment where I can count on my co-workers and they can count on me to solve real-world problems. 
                     Education and certifications in full-stack development and computer information systems from 
-                    University of California Irvine and Lewis & Clark Technical School. Skills in HTML, CSS, JavaScript, and Python.</p>
-                </div>
-               
-              </div>
-            
-            </div>
-          
-          </div>   
-      </main>
+                    University of California Irvine and Lewis & Clark Technical School. Skills in HTML, CSS, JavaScript, and Python..
+          </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
     );
 }
 
