01a3832168a2ec799dc168707d12db809cc51e89
Damon Burda <damonburda96@gmail.com>
2021-09-10 05:27:26 -0700

    font awesome icons on contact buttons

--- src/components/links/Links.js
+++ src/components/links/Links.js
@@ -1,14 +1,15 @@
 import React from "react";
-import {ButtonGroup, Button, Image} from "react-bootstrap";
-import Github from "./github_badge.png";
-import LinkedIn from "./linkedin_badge.png";
+import {ButtonGroup, Button } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+
 
 function Links () {
     return (
         <ButtonGroup size="lg" className="mb-2">
-          <Button variant="dark" size="sm" href="https://github.com/Damon1v1"><Image src={Github} ></Image></Button>
-          <Button variant="dark" size="sm" href="https://www.linkedin.com/in/damon-burda-4ab6a01b7/"><Image src={LinkedIn}></Image></Button>
-          <Button variant="primary" href="/contact" style={{ float: "right" }}>Contact Me</Button>
+          <Button variant="dark" size="sm" href="https://github.com/Damon1v1"><FontAwesomeIcon icon={faGithub}></FontAwesomeIcon> Github </Button>
+          <Button variant="dark" size="sm" href="https://www.linkedin.com/in/damon-burda-4ab6a01b7/"><FontAwesomeIcon icon={faLinkedin}></FontAwesomeIcon> LinkedIn</Button>
+          <Button variant="dark" href="/contact" style={{ float: "right" }}>Contact Me</Button>
         </ButtonGroup>
     );
 };
