996a538f8d302cffe901695f04fccfc0f74308fe
Damon Burda <damonburda96@gmail.com>
2021-09-10 05:51:35 -0700

    Added header above button group

--- src/components/links/Links.js
+++ src/components/links/Links.js
@@ -1,17 +1,26 @@
 import React from "react";
-import {ButtonGroup, Button } from "react-bootstrap";
+import {ButtonGroup, Button, Row } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 
 function Links () {
     return (
-        <ButtonGroup size="lg" className="mb-2">
-          <Button variant="dark" size="sm" href="https://github.com/Damon1v1"><FontAwesomeIcon icon={faGithub}></FontAwesomeIcon> Github </Button>
-          <Button variant="dark" size="sm" href="https://www.linkedin.com/in/damon-burda-4ab6a01b7/"><FontAwesomeIcon icon={faLinkedin}></FontAwesomeIcon> LinkedIn</Button>
-          <Button variant="dark" href="/contact" style={{ float: "right" }}>Contact Me</Button>
-        </ButtonGroup>
-    );
+      
+      <div>
+        <Row>
+          <h1> Get in touch with me! </h1>
+        </Row>
+
+        <Row>
+          <ButtonGroup size="lg" className="mb-2">
+            <Button variant="dark" size="sm" href="https://github.com/Damon1v1"><FontAwesomeIcon icon={faGithub}></FontAwesomeIcon> Github </Button>
+            <Button variant="dark" size="sm" href="https://www.linkedin.com/in/damon-burda-4ab6a01b7/"><FontAwesomeIcon icon={faLinkedin}></FontAwesomeIcon> LinkedIn</Button>
+            <Button variant="dark" href="/contact" style={{ float: "right" }}>Contact Me</Button>
+          </ButtonGroup>
+        </Row>
+      </div>
+  );
 };
 
 export default Links;
