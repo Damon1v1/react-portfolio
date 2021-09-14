0e177aa5393ea505d352a58e4eb811bd96b05b0c
Damon Burda <damonburda96@gmail.com>
2021-09-13 17:19:47 -0700

    project 1 image responsive, project 2 is not

--- src/pages/portfolio/portfolio.js
+++ src/pages/portfolio/portfolio.js
@@ -1,9 +1,9 @@
 import React from "react";
-import "./portfolio.css";
 import Project1 from "../../components/projects/Project1";
 import Project2 from "../../components/projects/Project2";
 import Movieferee from "./project1.png";
 import Kringle from "./project2.png";
+import "./portfolio.css"
 // import Project3 from "../../components/projects/project3"
 
 function Portfolio() {
@@ -17,7 +17,7 @@ function Portfolio() {
                     <div className="row">
                       <h2 className="col-sm-12" id="project-title">Movie Recommender</h2>
                       <div className="col-lg-12"></div>
-                      <img src={Movieferee} alt="Movie Recommender" className="img-fluid port-image" id="project-img" />
+                      <img src={Movieferee} alt="Movie Recommender" className="project1" id="project-img" />
                       <p id="project-description">Application that reccomends movies to the user to watch based on questionnaire responses;
                         you can view the respository and deployment of the live application 
                        <Project1></Project1>
@@ -27,7 +27,7 @@ function Portfolio() {
                     <div Name="row">
                       <h2 className="col-sm-12" id="project-title">Release The Kringle!</h2>
                       <div className="col-lg-12"></div>
-                      <img src={Kringle} alt="Release The Kringle home page" className="img-fluid port-image" id="project2-img" />
+                      <img src={Kringle} alt="Release The Kringle home page" className="project2" id="project2-img" />
                       <p id="project-description"> Mock-up application for a bakery. Allows users to sign in for rewards to be used on
                         various kringle products. Check out our repo and deployment
                         <Project2></Project2>
