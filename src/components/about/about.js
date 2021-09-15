255f2b02e093c9e7bd1b7ef4cb8d17f61575a89a
Damon Burda <damonburda96@gmail.com>
2021-09-15 16:19:29 -0700

    increased about me text size to be readable on desktop and mobile views

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -22,14 +22,14 @@ function About() {
           </Row>
             <Row><Card.Body>
             <Col xs={12}>
-            <Card.Text className="bio">
+            <Card.Text style={{ fontSize: "1.5rem" }}>
                       Web developer from a food service background consolidating leadership, teamwork skills, 
                       and quick problem solving ability from a fast-paced work environment to build a sleek and 
                       intuitive user experience on the web. Helped my restaurant achieve the fastest service times company wide 
                       in the third fiscal quarter of 2020 and number one overall restaurant in quarter four. I enjoy working in an 
                       environment where I can count on my co-workers and they can count on me to solve real-world problems. 
                       Education and certifications in full-stack development and computer information systems from 
-                      University of California Irvine and Lewis & Clark Technical School. Skills in HTML, CSS, JavaScript, and Python..
+                      University of California Irvine and Lewis & Clark Technical School. Skills in HTML, CSS, JavaScript, and Python.
             </Card.Text></Col>
         </Card.Body></Row>
       </Card>
