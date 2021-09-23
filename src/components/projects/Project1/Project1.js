4e67ce522a87c726c832c9daab88ec6280c1688a
Damon Burda <damonburda96@gmail.com>
2021-09-22 17:28:45 -0700

    project1 pane content

--- src/components/projects/Project1/Project1.js
+++ src/components/projects/Project1/Project1.js
@@ -1,7 +1,9 @@
 import React, {useState} from "react";
-import { Button } from "react-bootstrap";
+import { Button, Image } from "react-bootstrap";
 import SlidingPane from "react-sliding-pane";
 import "react-sliding-pane/dist/react-sliding-pane.css";
+import Project1a from "./Project1a.png";
+import Project1b from "./Project1b.png";
 import "../Pane.css";
 
 const Project1 = () => {
@@ -39,6 +41,35 @@ const Project1 = () => {
                         <li>Questionnaire that takes users inputs to grab query searches</li>
                         <li>Users answers plug in to API calls to find similar movies to their answers</li>
                         <li>Movies returned have their poster, rating, and summary appended to the page</li>
+                    </ul>
+                    <br/>
+                    <h2>Mock-Up</h2>
+                    <Image src={Project1a} className="Project1a"></Image>
+                    <Image src={Project1b} className="Project1b"></Image>
+                    <br/>
+                    <h2>Deployment</h2>
+                    <p>You can view the repository and deployment <a href="https://github.com/Damon1v1/Movie-Recommender"> here</a>.</p>
+                    <br/>
+                    <h2>Credits</h2>
+                    <h3>Damon Burda</h3>
+                    <ul>
+                        <li><a href="https://github.com/Damon1v1">Github</a></li>
+                        <li><a href="https://www.linkedin.com/in/damon-burda-4ab6a01b7/">LinkedIn</a></li>
+                    </ul>
+                    <h3>Noreldin Saad</h3>
+                    <ul>
+                        <li><a href="https://github.com/Noreldin-S">Github</a></li>
+                        <li><a href="https://www.linkedin.com/in/noreldin-saad-835930108/">LinkedIn</a></li>
+                    </ul>
+                    <h3>Ernie Perez</h3>
+                    <ul>
+                        <li><a href="https://github.com/jep1220?tab=repositories">Github</a></li>
+                        <li><a href="https://www.linkedin.com/in/juan-perez-aa2653139/">LinkedIn</a></li>
+                    </ul>
+                    <h3>Jessica Chen</h3>
+                    <ul>
+                        <li><a href="https://github.com/jiayc1">Github</a></li>
+                        <li><a href="https://www.linkedin.com/in/jchen52/">LinkedIn</a></li>
                     </ul>
                 </div>
             </SlidingPane>
