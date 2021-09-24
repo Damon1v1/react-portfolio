77a9000040541e4abb40ef3db91078a033a97589
Damon Burda <damonburda96@gmail.com>
2021-09-24 16:57:27 -0700

    project3 pane content

--- src/components/projects/Project3/Project3.js
+++ src/components/projects/Project3/Project3.js
@@ -1,6 +1,9 @@
 import React, {useState} from "react";
-import { Button } from "react-bootstrap";
+import { Button, Image } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTimesCircle } from "@fortawesome/free-solid-svg-icons";
 import SlidingPane from "react-sliding-pane";
+import Project3a from "./Project3a.png";
 import "react-sliding-pane/dist/react-sliding-pane.css";
 import "../Pane.css";
 
@@ -18,7 +21,7 @@ const Project3 = () => {
                 </Button>
             </div>
             <SlidingPane 
-            closeIcon={<div>X</div>}
+            closeIcon={<div><FontAwesomeIcon icon={faTimesCircle} /></div>}
             className="close"
             isOpen={state.isPaneOpenBottom}
             title="Bandstand"
@@ -26,11 +29,23 @@ const Project3 = () => {
             width="100%"
             onRequestClose={() => setState({ isPaneOpenBottom: false })}>
                 <div>
-                    <p>
-                        Lobby for users to connect with each other, chat and listen
-                        to music! You can view the repo and live deployment
-                        <a href="https://github.com/Damon1v1/Bandstand">here.</a>
-                    </p>
+                    <h1>MERN Stack application to chat with your friends while listening to music</h1>
+                    <br/>
+                    <h2>Features</h2>
+                    <ul>
+                        <li>Register as a new user</li>
+                        <li>Login Authentification</li>
+                        <li>Profile management</li>
+                        <li>Spotify listening with master control</li>
+                        <li>Chat pane for clean user interface</li>
+                        <li>Chat with your friends with socket.io!</li>
+                    </ul>
+                    <br/>
+                    <h2>Mock-Up</h2>
+                    <Image src={Project3a} className="Project3a" />
+                    <br/>
+                    <h2>Deployment</h2>
+                    <p>You can view the repository and live deployment <a href="https://github.com/Damon1v1/Bandstand"> here</a>.</p>
                 </div>
             </SlidingPane>
         </div>
