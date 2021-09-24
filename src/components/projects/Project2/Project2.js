4727b7af8aa5f375d83cfd6e0f583dd6cd79d5c5
Damon Burda <damonburda96@gmail.com>
2021-09-23 17:28:06 -0700

    project2 pane text content

--- src/components/projects/Project2/Project2.js
+++ src/components/projects/Project2/Project2.js
@@ -1,5 +1,7 @@
 import React, {useState} from "react";
-import { Button } from "react-bootstrap"; 
+import { Button, Image } from "react-bootstrap"; 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTimesCircle } from "@fortawesome/free-solid-svg-icons";
 import SlidingPane from "react-sliding-pane";
 import "react-sliding-pane/dist/react-sliding-pane.css";
 import "../Pane.css";
@@ -18,7 +20,7 @@ const Project2 = () => {
                 </Button>
             </div>
             <SlidingPane 
-            closeIcon={<div>X</div>}
+            closeIcon={<div><FontAwesomeIcon icon={faTimesCircle} /></div>}
             className="close"
             isOpen={state.isPaneOpenBottom}
             title="Release The Kringle!"
@@ -26,11 +28,22 @@ const Project2 = () => {
             width="100%"
             onRequestClose={() => setState({ isPaneOpenBottom: false })}>
                 <div>
-                    <p>
-                        Mock-up application for a bakery. Allows users to sign in for rewards to be used on
-                        various kringle products. Check out our repo and deployment 
-                        <a href="https://github.com/Damon1v1/Release-The-Kringle"> here</a>
-                    </p>
+                    <h1>Slepptu Kringlunni! The finest kringle bakery storefront</h1>
+                    <br/>
+                    <h2>Features</h2>
+                    <ul>
+                        <li>Home landing page for users to learn more about the bakery</li>
+                        <li>About us page for the user to learn about the developers and contact them by e-mail</li>
+                        <li>Bakery locations also listed on the about us page</li>
+                        <li>Dynamic menu that pulls kringles from our database</li>
+                        <li>Users can can sign up for 500 Kringle Kash for free</li>
+                        <li>Passwords are validated to be a certain length and stored hashed for security</li>
+                        <li>Users can also log back in on returning visists</li>
+                        <li>Kringle Rewards rewards page for logged in users to use their Kringle Kash</li>
+                    </ul>
+                    <br/>
+                    <h2>Deployment</h2>
+                    <p>You can view the repository and live deployment <a href="https://github.com/Damon1v1/Release-The-Kringle"> here</a>.</p>
                 </div>
             </SlidingPane>
         </div>
