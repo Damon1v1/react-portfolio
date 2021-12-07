638b101a192d61f9d84e7867fbdbb02922421802
Damon Burda <damonburda96@gmail.com>
2021-12-06 17:33:00 -0800

    styled project1 pane for readability

--- src/components/projects/Project1/Project1.js
+++ src/components/projects/Project1/Project1.js
@@ -46,8 +46,8 @@ const Project1 = () => {
                     </ul>
                     <br/>
                     <h2>Mock-Up</h2>
-                    <Image src={Project1a} className="Project1a"></Image>
-                    <Image src={Project1b} className="Project1b"></Image>
+                    <Image src={Project1a} id="Project1a"></Image>
+                    <Image src={Project1b} id="Project1b"></Image>
                     <br/>
                     <h2>Deployment</h2>
                     <p>You can view the repository and deployment <a href="https://github.com/Damon1v1/Movie-Recommender"> here</a>.</p>
