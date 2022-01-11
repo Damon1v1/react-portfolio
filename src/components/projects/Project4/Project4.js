b976ae2d9ee63ddb24699c8b2df1f50b7b2edecd
Damon Burda <damonburda96@gmail.com>
2022-01-10 17:48:02 -0800

    created panes for two more projects

---
+++ src/components/projects/Project4/Project4.js
@@ -0,0 +1,56 @@
+import React, {useState} from "react";
+import { Button, Image } from "react-bootstrap"; 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTimesCircle } from "@fortawesome/free-solid-svg-icons";
+import SlidingPane from "react-sliding-pane";
+import TechBlog from "./techblog.png";
+import "react-sliding-pane/dist/react-sliding-pane.css";
+import "../Pane.css";
+
+const Project4 = () => {
+    //this state might be for if another pane component is also in use
+    //might be a source for error in the future
+    const [state, setState] = useState({
+        isPaneOpen: false,
+    });
+    return (
+        <div>
+            <div>
+                <Button variant="dark" id="Button" onClick={() => setState({ isPaneOpen: true})}>
+                    Tech-Blog
+                </Button>
+            </div>
+            <SlidingPane 
+            closeIcon={<div><FontAwesomeIcon icon={faTimesCircle} /></div>}
+            className="close"
+            isOpen={state.isPaneOpen}
+            title="Tech-Blog"
+            from="bottom"
+            width="100%"
+            onRequestClose={() => setState({ isPaneOpen: false })}>
+                <div>
+                    <h1>CMS-style blog site</h1>
+                    <br/>
+                    <h2>Features</h2>
+                    <ul>
+                        <li>Homepage with navigation links</li>
+                        <li>Login and logout</li>
+                        <li>User credentials saved, hashed, and stored</li>
+                        <li>Existing blog posts shown on homepage with title and date created</li>
+                        <li>Option to leave comments on posts</li>
+                        <li>User can make unique posts</li>
+                        <li>User is taken back to updated dashboard after posting</li>
+                    </ul>
+                    <br/>
+                    <h2>Mock-Up</h2>
+                    <Image src={TechBlog} id="TechBlog" />
+                    <br/>
+                    <h2>Deployment</h2>
+                    <p>You can view the repository and live deployment <a href="https://github.com/Damon1v1/Tech-Blog"> here</a>.</p>
+                </div>
+            </SlidingPane>
+        </div>
+    )
+};
+
+export default Project4;
---
+++ src/components/projects/Project5/Project5.js
@@ -0,0 +1,56 @@
+import React, {useState} from "react";
+import { Button, Image } from "react-bootstrap"; 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTimesCircle } from "@fortawesome/free-solid-svg-icons";
+import SlidingPane from "react-sliding-pane";
+import Budget1 from "./Budget1.png";
+import Budget2 from "./Budget2.png"
+import "react-sliding-pane/dist/react-sliding-pane.css";
+import "../Pane.css";
+
+const Project5 = () => {
+    //this state might be for if another pane component is also in use
+    //might be a source for error in the future
+    const [state, setState] = useState({
+        isPaneOpen: false,
+    });
+    return (
+        <div>
+            <div>
+                <Button variant="dark" id="Button" onClick={() => setState({ isPaneOpen: true})}>
+                    Budget Tracker
+                </Button>
+            </div>
+            <SlidingPane 
+            closeIcon={<div><FontAwesomeIcon icon={faTimesCircle} /></div>}
+            className="close"
+            isOpen={state.isPaneOpen}
+            title="Budget Tracker"
+            from="bottom"
+            width="100%"
+            onRequestClose={() => setState({ isPaneOpen: false })}>
+                <div>
+                    <h1>Manage your expenses</h1>
+                    <br/>
+                    <h2>Features</h2>
+                    <ul>
+                        <li>Online and offline functionality</li>
+                        <li>Add or subtract funds</li>
+                        <li>Chart to visualize your transaction history</li>
+                        <li>Funds are updated once a user reconnects to the internet</li>
+                        <li>Named entries to track transactions</li>
+                    </ul>
+                    <br/>
+                    <h2>Mock-Up</h2>
+                    <Image src={Budget1} id="Transactions" />
+                    <Image src={Budget2} id="Graph" />
+                    <br/>
+                    <h2>Deployment</h2>
+                    <p>You can view the repository and live deployment <a href="https://github.com/Damon1v1/Online-Offline-Budget-Tracker"> here</a>.</p>
+                </div>
+            </SlidingPane>
+        </div>
+    )
+};
+
+export default Project5;
