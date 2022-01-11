f6134b0deea47ed91fc88737522e02ecb9869ab5
Damon Burda <damonburda96@gmail.com>
2022-01-10 16:39:18 -0800

    page content fades in on scorll

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,22 +1,27 @@
-import  React from "react";
+import  React, { useEffect } from "react";
 import { Row } from "react-bootstrap";
 import "./home.css";
 import "../../components/about/About";
 import About from "../../components/about/About";
 import Portfolio from "../portfolio/Portfolio";
 import Links from "../../components/links/Links";
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 
-function Home() {
+const Home = () => {
+    useEffect(() => {
+        Aos.init({duration: 2000});
+    }, []);
     return(
         <div>
-            <div>
+            <div data-aos="fade">
                 <Row className="justify-content-center">
                     <About />
                 </Row>
             </div>
 
-            <div>
+            <div data-aos="fade">
                 <Row>
                     <Portfolio />
                 </Row>
