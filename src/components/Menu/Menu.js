4728fe4495e0b626d2dae25a392b40f38d86d550
Damon Burda <damonburda96@gmail.com>
2022-01-12 19:22:41 -0800

    redone and debugged navigation dropdown

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,25 +1,17 @@
 import React, { Component } from "react";
-import { Dropdown } from "react-bootstrap";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { Dropdown, DropdownButton } from "react-bootstrap";
 import "./menu.css";
-import {Link } from 'react-scroll';
+import { Link } from 'react-scroll';
 
 class Menu extends Component {  
-    render () {
-      return (
-        <Dropdown id="menu">
-        <Dropdown.Toggle variant="Secondary" id="dropdown-basic" size="lg">
-          <FontAwesomeIcon icon={faBars} />
-        </Dropdown.Toggle>
-
-        <Dropdown.Menu>
-          <Link to="portfolio" spy={true} smooth={true}><Dropdown.Item id="item">Portfolio</Dropdown.Item></Link>
-          <Link to="contact" spy={true} smooth={true}><Dropdown.Item id="item">Contact</Dropdown.Item></Link>
-        </Dropdown.Menu>
-      </Dropdown>
-      )
-    }
+  render () {
+    return (
+      <DropdownButton id="menu" title="Jump" variant="Success">
+        <Link to="portfolio" spy={true} smooth={true}><Dropdown.Item as="button">Portfolio</Dropdown.Item></Link>
+        <Link to="contact" spy={true} smooth={true}><Dropdown.Item as="button">Contact</Dropdown.Item></Link>
+      </DropdownButton>
+    )
+  }
 };
 
 export default Menu;
