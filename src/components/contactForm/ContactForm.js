74d795d2d5e104810f749024ff85e1530221b461
Damon1v1 <damonburda96@gmail.com>
2022-01-24 18:28:01 -0800

    emailjs adjustments

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -18,13 +18,17 @@ function ContactForm() {
 
     return (
       <Form className="ContactForm" onSubmit={sendEmail}>
+        <Form.Group className="mb-3">
+          <Form.Label>Name</Form.Label>
+          <Form.Control type="name" placeholder="John Doe" name="name" />
+        </Form.Group>
         <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="name@example.com" />
+          <Form.Control type="email" placeholder="name@example.com" name="email"/>
         </Form.Group>
         <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
           <Form.Label>Message</Form.Label>
-          <Form.Control as="textarea" rows={3} />
+          <Form.Control as="textarea" rows={3} name="message"/>
         </Form.Group>
         <Button variant="dark" type="submit" className="btn-lg" id="submit">
           Submit
